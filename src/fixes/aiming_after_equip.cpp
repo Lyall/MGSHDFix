115a2587113f59ac18e4647e9cbc446a82f9fa63
qh236 <qureshih@oregonstate.edu>
2025-08-07 15:07:35 -0400

    Aim After Equip Fix

--- src/fixes/aiming_after_equip.cpp
+++ src/fixes/aiming_after_equip.cpp
@@ -14,10 +14,19 @@ void FixAimAfterEquip::Initialize()
 
     if (eGameType & MGS2)
     {
-        MAKE_HOOK_MID(baseModule, "89 1D ?? ?? ?? ?? 8B C3 48 83 C4", "MGS2: Aiming After Equip", {
+        MAKE_HOOK_MID(baseModule, "89 1D ?? ?? ?? ?? 8B C3 48 83 C4", "Snake's Hook", {
             g_GameVars.SetAimingState(0);
-        })
-    }
+        });
+
+        MAKE_HOOK_MID(baseModule, "89 B3 0C 02 00 00", "Raiden's Hook", {
+            if ((uint32_t)ctx.rsi == 0)
+            {
+             g_GameVars.SetAimingState(0);
+           }
+        });
+
+        }
+    
     else if (eGameType & MGS3)
     {
         MAKE_HOOK_MID(baseModule, "44 89 0D ?? ?? ?? ?? EB ?? 48 8D 05", "MGS3: Aiming After Equip", {
@@ -25,7 +34,8 @@ void FixAimAfterEquip::Initialize()
             {
                 g_GameVars.SetAimingState(0);
             }
-            })
+            });
     }
-
 }
+
+
