eb0337ec00c0ae5fbf8f0fd117150eab674ae164
Afevis <ShizCalev@users.noreply.github.com>
2025-08-08 04:39:22 -0600

    Update src/fixes/aiming_after_equip.cpp

--- src/fixes/aiming_after_equip.cpp
+++ src/fixes/aiming_after_equip.cpp
@@ -18,7 +18,7 @@ void FixAimAfterEquip::Initialize()
             g_GameVars.SetAimingState(0);
         });
 
-        MAKE_HOOK_MID(baseModule, "89 B3 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 4C 8B 74 24, "MGS2: Aiming After Equip - Raiden", {
+        MAKE_HOOK_MID(baseModule, "89 B3 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 4C 8B 74 24", "MGS2: Aiming After Equip - Raiden", {
             if ((uint32_t)ctx.rsi == 0)
             {
                 g_GameVars.SetAimingState(0);
