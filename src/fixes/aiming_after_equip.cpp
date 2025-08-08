cf6f6855ad99e52905864c92622e0c43f0ae6073
Afevis <ShizCalev@users.noreply.github.com>
2025-08-08 04:28:43 -0600

    Apply suggestions from code review

--- src/fixes/aiming_after_equip.cpp
+++ src/fixes/aiming_after_equip.cpp
@@ -14,11 +14,11 @@ void FixAimAfterEquip::Initialize()
 
     if (eGameType & MGS2)
     {
-        MAKE_HOOK_MID(baseModule, "89 1D ?? ?? ?? ?? 8B C3 48 83 C4", "Snake's Hook", {
+        MAKE_HOOK_MID(baseModule, "89 1D ?? ?? ?? ?? 8B C3 48 83 C4", "MGS2: Aiming After Equip - Snake", {
             g_GameVars.SetAimingState(0);
         });
 
-        MAKE_HOOK_MID(baseModule, "89 B3 0C 02 00 00", "Raiden's Hook", {
+        MAKE_HOOK_MID(baseModule, "89 B3 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 4C 8B 74 24, "MGS2: Aiming After Equip - Raiden", {
             if ((uint32_t)ctx.rsi == 0)
             {
              g_GameVars.SetAimingState(0);
