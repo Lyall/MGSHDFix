6e9981f82ae17f08d903fa80cdd173c1d14ce5e1
Afevis <ShizCalev@users.noreply.github.com>
2025-08-24 13:07:41 -0600

    Update distance_culling.cpp

--- src/features/distance_culling.cpp
+++ src/features/distance_culling.cpp
@@ -14,7 +14,7 @@ void DistanceCulling::Initialize() const
         if (bForceGrassAlways || fGrassDistanceScalar != 1.0f)
         {
             MAKE_HOOK_MID(baseModule, "F3 0F 11 83 ?? ?? ?? ?? 41 8B FC", "MGS3: Grass Farclip", {
-               ctx.xmm0.f32[0] = g_DistanceCulling.bForceGrassAlways ? std::numeric_limits<float>::max() : g_DistanceCulling.fGrassDistanceScalar;
+               ctx.xmm0.f32[0] = g_DistanceCulling.bForceGrassAlways ? std::numeric_limits<float>::max() : ctx.xmm0.f32[0] * g_DistanceCulling.fGrassDistanceScalar;
                 })
 
             if (bForceGrassAlways)
