8c5fdfbc3103ef3f05440cdc4feffba638691109
Afevis <ShizCalev@users.noreply.github.com>
2026-05-08 12:51:20 -0600

    mem sigs for snakearm_voice fix

--- src/fixes/snakearm_voice.cpp
+++ src/fixes/snakearm_voice.cpp
@@ -21,14 +21,14 @@ void SnakeArmFixes::ApplyFixes() {
     // Get SOL_SolControl (unsure how to properly optimize the pattern, more context attached)
     //   4B 8B CD 45 8D 41 EC E8 ?? ?? ?? ?? 44 8B 8D D8 00 00 00 
     //   41 B8 0F 00 00 00 8B 8D C4 00 00 00 48 8B D5 E8 ?? ?? ?? ?? A8 01 74 07 E8 ?? ?? ?? ?? 
-    uint8_t* Solidus_InitControl = Memory::PatternScan(baseModule, "EB 05 F3 0F 10 47 64 F3 0F 10 0D ?? ?? ?? ?? 0F 28 D0 48 8B CD E8 ?? ?? ?? ?? 8B D6 48 89 2D", "Solidus Voice Position");
+    uint8_t* Solidus_InitControl = Memory::PatternScan(baseModule, "EB ?? F3 0F 10 47 ?? F3 0F 10 0D ?? ?? ?? ?? 0F 28 D0", "Solidus Voice Position");
     SolControl = (uintptr_t*)Memory::GetRelativeOffset(Solidus_InitControl + 31);
 
     // user/sonoyama/solidus/snakearm.c -> ClawAttackHit - uses GM_PlayerPosition instead of SOL_SolControl->mov
     // Same for OnlineHit
     // This is probably not the correct way to change an offset from one global to another.
-    uint8_t* ClawAttackHit = Memory::PatternScan(baseModule, "2B C8 48 8D 15 ?? ?? ?? ?? 8B C1 48 8D 0D ?? ?? ?? ?? 8B 0C 81 48 8B", "Solidus Voice Position Fix");
-    uint8_t* OnlineHit = Memory::PatternScan(baseModule, "2B C8 48 8D 15 ?? ?? ?? ?? 8B C1 48 8D 0D ?? ?? ?? ?? 8B 0C 81 E8", "Solidus Voice Position Fix 2");
+    uint8_t* ClawAttackHit = Memory::PatternScan(baseModule, "2B C8 48 8D 15 ?? ?? ?? ?? 8B C1 48 8D 0D ?? ?? ?? ?? ?? ?? ?? 48 8B 5C 24", "Solidus Voice Position Fix");
+    uint8_t* OnlineHit = Memory::PatternScan(baseModule, "2B C8 48 8D 15 ?? ?? ?? ?? 8B C1 48 8D 0D ?? ?? ?? ?? ?? ?? ?? E8", "Solidus Voice Position Fix 2");
 
     if (Solidus_InitControl && ClawAttackHit && OnlineHit) {
         // int32_t SolControl_Relative = (int32_t)(/*(long)&SolControl*/ (uintptr_t)SolControl + 0x40 - (long)ClawAttackHit - 9);
