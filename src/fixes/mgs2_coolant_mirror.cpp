fea71675dff1c42c6df43e57bdd4c594e7c66d59
Jacky720 <32578221+Jacky720@users.noreply.github.com>
2026-05-13 10:34:24 -0400

    Restore inverted thermal for fog

--- src/fixes/mgs2_coolant_mirror.cpp
+++ src/fixes/mgs2_coolant_mirror.cpp
@@ -6,7 +6,9 @@
 
 #include "logging.hpp"
 
-
+namespace {
+    short** GM_Item;
+}
 
 void CoolantMirrorFix::ApplyFix()
 {
@@ -23,5 +25,22 @@ void CoolantMirrorFix::ApplyFix()
         ((float*)ctx.rcx)[1] /= 2.0f;
         ((float*)ctx.rcx)[2] /= 2.0f;
     });
+
+    // Grab GM_Item out of some Olga code, why not
+    if (uint8_t* OlgaMouthDisp = Memory::PatternScan(baseModule, "F7 81 E0 13 00 00 00 00 00 08 75 3A 48 8B 05", "MGS2: Coolant Mirror")) {
+        //uintptr_t globsBase = *(uintptr_t*)Memory::GetRelativeOffset(OlgaMouthDisp + 15);
+        //GM_Item = (short*)(globsBase + 0x106);
+        GM_Item = (short**)Memory::GetRelativeOffset(OlgaMouthDisp + 15);
+        // Xbox ifdefed feature: Different alpha settings in thermal vision
+        MAKE_HOOK_MID(baseModule, "44 8B 43 ?? B8", "MGS2: Coolant Mirror", {
+            const auto prim = *reinterpret_cast<uintptr_t*>(ctx.rbx + 88);
+            *reinterpret_cast<uint64_t*>(prim + 0x200) = 
+                (GM_Item[0][0x83] == 13) // GM_Item == IT_Thermal
+                    ? 0x42ULL    // SCE_GS_SET_ALPHA(2, 0, 0, 1, 0)
+                    : 0x44ULL;   // SCE_GS_SET_ALPHA(0, 1, 0, 1, 0)
+        });
+
+    }
+
 
 }
