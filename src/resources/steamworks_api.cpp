def16c44235e7592fe34c92a83b33d14b032219c
Afevis <ShizCalev@users.noreply.github.com>
2026-05-18 16:42:09 -0600

    Detect unbound steam input menu buttons

--- src/resources/steamworks_api.cpp
+++ src/resources/steamworks_api.cpp
@@ -6,9 +6,11 @@
 
 #pragma warning(push)
 #pragma warning(disable:4828)
+#include "config_keys.hpp"
 #include "isteamuser.h"
 #include "isteamuserstats.h"
 #include "isteaminput.h"
+#include "swap_menu_buttons.hpp"
 #include "version.h"
 
 #pragma warning(pop)
@@ -488,13 +490,32 @@ void SteamAPI::OnSteamInputLoaded()
             InputDigitalActionHandle_t actionHandle = steamInput->GetDigitalActionHandle(actionName.c_str());
             if (actionHandle == 0)
             {
-                spdlog::error("SteamInput: Game Action '{}' not bound for Controller #{}", actionName, i + 1);
+                spdlog::error("SteamInput: Controller #{} | Game Action '{}' does not exist in the Steam Input manifest", i + 1, actionName);
+
                 bHasUnboundButtons = true;
                 continue;
             }
 
             EInputActionOrigin origins[STEAM_INPUT_MAX_ORIGINS] = {};
             int originCount = steamInput->GetDigitalActionOrigins(handle, activeActionSet, actionHandle, origins);
+
+            if (originCount <= 0)
+            {
+                spdlog::error("SteamInput: Controller #{} | Game Action (0x{:X}) '{}' is UNBOUND in the active action set", i + 1, actionHandle, actionName);
+
+                if ((SwapMenuButtons::force_menu_buttons != ConfigKeys::MenuButton_Option_Default) && ((actionName == "ingame_cmn_punch") || (actionName == "ingame_cmn_sneaking")))
+                {
+                    spdlog::error("-------------------    ERROR     ----------------------");
+                    spdlog::error("SteamInput: '{}' is not bound in Steam Input.", actionName);
+                    spdlog::error("SteamInput: This is typically caused by using an outdated community-made controller layout/profile that swapped the binds to keyboard inputs.");
+                    spdlog::error("SteamInput: MGSHDFix's swapped menu buttons may not function correctly.");
+                    spdlog::error("SteamInput: Rebind this Steam Input Game Action or switch Menu Buttons back to Default for proper functionality.");
+                    spdlog::error("-------------------    ERROR     ----------------------");
+                }
+
+                bHasUnboundButtons = true;
+                continue;
+            }
 
             for (int j = 0; j < originCount; ++j)
             {
