4b589e6b24f28e7ee2bceaccab390271a7ba6bf9
Afevis <ShizCalev@users.noreply.github.com>
2026-05-30 04:26:06 -0600

    Update mgs2_parrot_radar_fix.cpp

--- src/fixes/mgs2_parrot_radar_fix.cpp
+++ src/fixes/mgs2_parrot_radar_fix.cpp
@@ -11,9 +11,7 @@ void MGS2_ParrotRadarFix::Apply()
     }
 
     MAKE_HOOK_MID(baseModule, "8B 83 ?? ?? ?? ?? 85 C0 75 ?? 8B 83", "MGS2: Parrot Fix", {
-        *reinterpret_cast<int16_t*>(ctx.rbx + 0x3B4) =                                                                                          // npc->action.face_dir = 
-                                                        *reinterpret_cast<int16_t*>(*reinterpret_cast<uintptr_t*>(ctx.rbx + 0x358) + 0x22)                                // ctrl->rot.vy +
-                                                                                                                                          + *reinterpret_cast<int16_t*>(ctx.rbx + 0xA02);   // svecAdjust.vy
+        *reinterpret_cast<int16_t*>(ctx.rbx + 0x3B4)    =   *reinterpret_cast<int16_t*>(*reinterpret_cast<uintptr_t*>(ctx.rbx + 0x358) + 0x22)  +   *reinterpret_cast<int16_t*>(ctx.rbx + 0xA02); // npc->action.face_dir = ctrl->rot.vy + svecAdjust.vy
                   });
 
 }
