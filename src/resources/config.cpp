bcebe2adc25ed2591a35e68f320274fb3a33ea9a
Paul Connolly <paulconnolly@gmail.com>
2026-08-12 21:29:51 +0100

    MGS2: restore censored demo effects (#354)
    * MGS2: restore censored demo effects
    
    * MGS2: gate the censored demo effects behind a photosensitivity toggle

--- src/resources/config.cpp
+++ src/resources/config.cpp
@@ -18,6 +18,7 @@
 #include "mgs2_gas_haze.hpp"
 #include "mgs2_demo_blur.hpp"
 #include "mgs2_demo_camera_judder.hpp"
+#include "mgs2_demo_effect_blacklist.hpp"
 #include "mgs2_reverb_wet_level.hpp"
 #include "mgs2_preshade_lights.hpp"
 #include "mgs2_tanker_snake_snap.hpp"
@@ -719,6 +720,11 @@ void Config::Read()
         if (eGameType & MGS2)
         {
             g_MGS2UnderwaterFilterFix.bEnabled = g_OpticalCamoFix.bEnabled = MGS2BloodStains::bEnabled = MGS2ScopeWarp::bEnabled = MGS2WaterEffects::bEnabled = MGS2LensDroplets::bEnabled = MGS2GasHaze::bEnabled = MGS2_ContrastShader::bEnabled = MGS2_AiRayVision::bEnabled = MGS2_Crossfade::bEnabled = MGS2ConcentrateBlur::bEnabled = MGS2RailgunBeam::bEnabled = MGS2DemoCameraJudder::bEnabled = MGS2PreshadeLights::bEnabled = MGS2TankerSnakeSnap::bEnabled = MGS2HairLayering::bEnabled = MGS2_CodecBackground::bEnabled = bRestoreVFX;
+
+            bool bDisablePhotosensitive = true;
+            ConfigHelper::getValue(ini, ConfigKeys::MGS2_Photosensitive_Section, ConfigKeys::MGS2_Photosensitive_Setting, bDisablePhotosensitive);
+            MGS2DemoEffectBlacklist::bEnabled = !bDisablePhotosensitive;
+            LOG_CONFIG(ConfigKeys::MGS2_Photosensitive_Section, ConfigKeys::MGS2_Photosensitive_Setting, bDisablePhotosensitive);
 
             std::string sMotionBlur;
             ConfigHelper::getValue(ini, ConfigKeys::MotionBlur_Section, ConfigKeys::MotionBlur_Setting, sMotionBlur);
